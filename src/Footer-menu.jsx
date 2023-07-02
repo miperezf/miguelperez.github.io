0ab50095135e1b869b1c0295ab840d046a4c84bc
Miguel Perez <miguuuel.perez@gmail.com>
2023-07-01 21:10:59 -0400

    corrigeenlancegithub

--- src/Footer-menu.jsx
+++ src/Footer-menu.jsx
@@ -5,7 +5,7 @@ export default function Footermenu() {
         <ul className="footer-menu">
             <li><a href="https://www.instagram.com/v.oo1d"><span>Instagram</span><img src="https://cdn2.iconfinder.com/data/icons/social-media-pixel/765/instagram-512.png" alt="" /></a></li>
             <li><a href="https://www.linkedin.com/in/miperezf"><span>Linkedin</span><img src="https://cdn2.iconfinder.com/data/icons/social-media-pixel/765/linked_in-512.png" alt="" /></a></li>
-            <li><a href="https://www.linkedin.com/in/miperezf"><span>Github</span><img src="https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d7e4e1e509c728d.png" alt="" /></a></li>
+            <li><a href="https://github.com/miperezf"><span>Github</span><img src="https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d7e4e1e509c728d.png" alt="" /></a></li>
         </ul>
         <div className="developed-span">
         <span>Developed by Vo1d © Copyright 2023</span>
