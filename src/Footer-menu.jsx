554fda9903a442a35e0494b922fdab5557a86203
Miguel Perez <miguuuel.perez@gmail.com>
2023-07-01 21:08:17 -0400

    cargaimagenesfooter

--- src/Footer-menu.jsx
+++ src/Footer-menu.jsx
@@ -3,9 +3,9 @@ export default function Footermenu() {
       <>
   <footer className="footer-container">
         <ul className="footer-menu">
-            <li><a href="https://www.instagram.com/v.oo1d"><span>Instagram</span><img src="./src/img/8495167_instagram_photo_icon.png" alt="" /></a></li>
-            <li><a href="https://www.linkedin.com/in/miperezf"><span>Linkedin</span><img src="./src/img/8495163_linked_in_professional_business_linkedin_icon.png" alt="" /></a></li>
-            <li><a href="https://www.linkedin.com/in/miperezf"><span>Github</span><img src="./src/img/d7e4e1e509c728d.png" alt="" /></a></li>
+            <li><a href="https://www.instagram.com/v.oo1d"><span>Instagram</span><img src="https://cdn2.iconfinder.com/data/icons/social-media-pixel/765/instagram-512.png" alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/miperezf"><span>Linkedin</span><img src="https://cdn2.iconfinder.com/data/icons/social-media-pixel/765/linked_in-512.png" alt="" /></a></li>
+            <li><a href="https://www.linkedin.com/in/miperezf"><span>Github</span><img src="https://pixelartmaker-data-78746291193.nyc3.digitaloceanspaces.com/image/d7e4e1e509c728d.png" alt="" /></a></li>
         </ul>
         <div className="developed-span">
         <span>Developed by Vo1d © Copyright 2023</span>
